761cd278feae103ed53dd2773a9f1213321d71ed
JorgeAlcaldeG <jorge-2806@hotmail.com>
2024-02-17 20:49:23 +0100

    Added favicon and readjustment in login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,10 +1,21 @@
 @extends('layouts.plantilla_login')
 
-@section('titulo','Login')
+@section('titulo','The Foot - Login')
 
 @section('content')
     <div id="logContainer">
         <h1 id="loginTitle" class="roboto-bold text-align">Login</h1>
-        <p class="text-align subTitule">Inicia sesión para descubrir y reservar el mejor restaurante</p>
+        <p class="text-align subTitule roboto-light-italic">Inicia sesión para descubrir y reservar el mejor restaurante</p>
+        <form action="" method="post" id="loginForm">
+            <label for="email" class="roboto-black">Correo electrónico</label>
+            <br>
+            <input type="text" name="email" id="email">
+            <br>
+            <label for="pwd" class="roboto-black">Contraseña</label>
+            <br>
+            <input type="password" name="pwd" id="pwd">
+            <br>
+            <button type="submit" id="loginbtn">Iniciar sesión</button>
+        </form>
     </div>
 @endsection
