a63736e6fd2f9d96b17b3ba610120d8a99d71f1b
Ricard Casals <richicasals@gmail.com>
2024-02-19 20:49:45 +0100

    Created restaurants API

--- app/Http/Controllers/AdminRestaurantController.php
+++ app/Http/Controllers/AdminRestaurantController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 use App\Models\Restaurant;
 use App\Models\User;
+use App\Models\Valoration;
+use App\Models\RestaurantImage;
 
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdminRestaurantController extends Controller
 {
@@ -32,27 +36,144 @@ class AdminRestaurantController extends Controller
             'thumbnail' => 'required|image|mimes:jpeg,png,jpg,gif,svg',
         ]);
 
-        $restaurant = new Restaurant;
+        try {
+            DB::beginTransaction();
 
-        $image = $request->file('thumbnail');
-        $imageName = time() . '.' . $image->getClientOriginalExtension();
-        $image->move(public_path('images/thumbnails'), $imageName);
+            $restaurant = new Restaurant;
 
-        $manager = User::find($request->manager_id);
+            // Store the thumbnail
+            $thumbnail = $request->file('thumbnail');
+            $thumbnail_name = time() . '.' . $thumbnail->getClientOriginalExtension();
+            $thumbnail->move(public_path('images/thumbnails'), $thumbnail_name);
+    
+            $manager = User::find($request->manager_id);
+    
+            // Create the restaurant
+            $restaurant->name = $request->name;
+            $restaurant->description = $request->description;
+            $restaurant->location = $request->location;
+            $restaurant->average_price = $request->average_price;
+            $restaurant->status = $request->status;
+            $restaurant->manager_id = $manager->id;
+            $restaurant->thumbnail = $thumbnail_name;
+            $restaurant->save();
+    
+            // Store the restaurant images
+            $images = $request->file('images');
+            foreach ($images as $image) {
+                $rand = substr(uniqid('', true), -5);
+                $images_name = $rand . "_" . time() . '.' . $image->getClientOriginalExtension();
+                $image->move(public_path('images/restaurants'), $images_name);
+    
+                $restaurant_image = new RestaurantImage;
+                $restaurant_image->restaurant_id = $restaurant->id;
+                $restaurant_image->image_url = $images_name;
+                $restaurant_image->save();
+            }
 
-        $restaurant->name = $request->name;
-        $restaurant->description = $request->description;
-        $restaurant->location = $request->location;
-        $restaurant->average_price = $request->average_price;
-        $restaurant->status = $request->status;
-        $restaurant->manager_id = $manager->id;
-        $restaurant->thumbnail = $imageName;
-        $restaurant->save();
+            // Create the restaurant foodtypes
+            $foodtypes = $request->foodtypes;
+            foreach ($foodtypes as $foodtype) {
+                $restaurant->foodtypes()->attach($foodtype);
+            }
 
-        return "creado papi";
+            DB::commit();
+    
+            return "creado papi";
+
+        } catch (Exception $e) {
+            DB::rollBack();
+            return "error";
+        }
+
     }
 
-    public function formejemplo() {
-        return view("formejemplo");
+    public function update($id, Request $request) {
+        $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required|max:255',
+            'location' => 'required|max:255',
+            'average_price' => 'required|integer',
+            'status' => 'required|integer',
+            'manager_id' => 'required|integer',
+        ]);
+
+        try {
+            DB::beginTransaction();
+
+            $restaurant = Restaurant::find($id);
+
+            $manager = User::find($request->manager_id);
+
+            // Update restaurant
+            $restaurant->name = $request->name;
+            $restaurant->description = $request->description;
+            $restaurant->location = $request->location;
+            $restaurant->average_price = $request->average_price;
+            $restaurant->status = $request->status;
+            $restaurant->manager_id = $manager->id;
+
+            // Update restaurant thumbnail
+            if ($request->hasFile('thumbnail')) {
+                $thumbnail = $request->file('thumbnail');
+                $thumbnail_name = time() . '.' . $thumbnail->getClientOriginalExtension();
+                $thumbnail->move(public_path('images/thumbnails'), $thumbnail_name);
+                $restaurant->thumbnail = $thumbnail_name;
+            }
+
+            $restaurant->save();
+
+            // Update the restaurant foodtypes
+            $restaurant->foodtypes()->detach();
+            $foodtypes = $request->foodtypes;
+            foreach ($foodtypes as $foodtype) {
+                $restaurant->foodtypes()->attach($foodtype);
+            }
+
+            DB::commit();
+    
+            return "modificado";
+
+        } catch (Exception $e) {
+            DB::rollBack();
+            return "error";
+        }
+    }
+
+
+    public function destroy($id) {
+        try {
+            DB::beginTransaction();
+            
+            // Delete restaurant valorations
+            $valorations = Valoration::where('restaurant_id', $id);
+            $valorations->delete();
+
+            // Delete restaurant images
+            $restaurant_images = RestaurantImage::where('restaurant_id', $id);
+            $restaurant_images->delete();
+
+            $restaurant = Restaurant::find($id);
+            // Delete restaurant foodtypes
+            $restaurant->foodtypes()->detach();
+            // Delete restaurant
+            $restaurant->delete();
+
+            DB::commit();
+
+            return "eliminado";
+
+        } catch (Exception $e) {
+            DB::rollBack();
+            return "error";
+        }
+
+    }
+
+    
+    public function formejemplo($id) {
+        $restaurant = Restaurant::find($id);
+
+        return view('formejemplo', compact('restaurant'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,14 @@ Route::get('/', function () {
 Route::controller(RestaurantController::class)->group(function () {
     Route::get('/api/restaurants', 'list')->name('api.restaurants.list');
     Route::get('/api/restaurants/{id}', 'show')->name('api.restaurants.show');
+
+    /*     Route::get('cursos', 'index')->name('cursos.index');
+    Route::get('cursos/{id}', 'show')->name('cursos.show');
+    Route::delete('cursos/{curso}', 'destroy')->name('cursos.destroy');
+    Route::put('cursos/{curso}', 'update')->name('cursos.update');
+    Route::get('cursos/create', 'create')->name('cursos.create');
+    Route::get('cursos/edit/{id}', 'edit')->name('cursos.edit');
+    Route::post('cursos/store', 'store')->name('cursos.store'); */
 });
 
 
@@ -31,6 +39,10 @@ Route::controller(AdminRestaurantController::class)->group(function () {
     Route::get('/api/admin/restaurants', 'list')->name('api.admin.restaurants.list');
     Route::get('/api/admin/restaurants/{id}', 'show')->name('api.admin.restaurants.show');
     Route::post('/api/admin/restaurants/store', 'store')->name('api.admin.restaurants.store');
+    Route::delete('/api/admin/restaurants/{id}', 'destroy')->name('api.admin.restaurants.destroy');
+    Route::put('/api/admin/restaurants/{id}', 'update')->name('api.admin.restaurants.update');
+    
+    Route::get('/api/admin/formejemplo/{id}', 'formejemplo')->name('api.admin.formejemplo');
 });
 
 
